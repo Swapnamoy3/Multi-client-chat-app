0d4cc0cd204d29fcf56461627ba1e3e2cc9fb6d6
swapnamoy garai <garaiswap27@gmail.com>
2025-03-09 20:48:22 +0530

    setted up a jwt , hasshing and storing the cookie in to http only cookie and also aded a database like thing that can for not temporarily store the user data

--- server/app.js
+++ server/app.js
@@ -4,7 +4,10 @@ const http = require('http')
 const {Server} = require("socket.io")
 const log = require("./logger.js");
 const setupIO = require("./io.js");
-
+const { createUser, saveUser, getUser, updateUser } = require("./database.js");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
@@ -17,13 +20,73 @@ const io = new Server(server, {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.post("/signup",(req,res) => {
+
+const JWT_SECRET = "mySecret";
+
+function generateToken(user){
+    return  jwt.sign({
+        id: user.id,
+        email: user.email
+    }, 
+        JWT_SECRET,{
+            expiresIn: "1h"
+        }
+    )
+}
+
+
+app.post("/signup",async (req,res) => {
     console.log("This is a body:")
     console.log(req.body);
+    const {name, email, password} = req.body;
+    name = name.trim().toLowerCase();
+    email = email.trim().toLowerCase();
+    password = password.trim();
+
+    if(!name || !email || !password){
+        res.status(400).json({status: 400, message: "Invalid SignUp Info"});
+    }
+    
+    if(getUser(email)){
+        res.status(400).json({status: 400, message: "User Already exist"});
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+
+    const user = createUser(name, email, hashedPassword);
+    saveUser(user);
 
     res.json({status: "ok", message: "YOu have been signed up and Logged in"});
 })
+
+app.post("/login", async (res,req) => {
+    const {email, password} = res.body;
+    const user = getUser(email);
+    if(!user){
+        return res.status(401).json({ status: 401, message: "Email not registered" });
+    }
+
+    const passwordMatching = await bcrypt.compare(password, user.password);
+    if(!passwordMatching){
+        return res.status(401).json({ status: 401, message: "Invalid credentials" });
+    }
+
+
+    const token = generateToken(user);
+
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: false, 
+        sameSite: "Strict",
+        maxAge: 1000 * 60 * 60 * 1 // becuase 1 hour is the expire time in jwt token
+    })
+
+    res.json({status: "ok", message: "You have been logged in"});
+})
+
 
 
 setupIO(io);
---
+++ server/database.js
@@ -0,0 +1,48 @@
+const { v4: uuidv4 } = require('uuid');
+
+const users = [];
+/*
+user = {
+    id: string,
+    name: string,
+    email: string,
+    password: string, 
+    backupEmail: string,
+    backupPhone: string
+}
+*/
+function createUser(name, email, password, backupEmail = "", backupPhone = "") {
+    return {
+        id: uuidv4(),
+        name: name,
+        email: email,
+        password: password,
+        backupEmail: backupEmail,
+        backupPhone: backupPhone,
+        socketID : null
+    }
+}
+
+function saveUser(user) {
+    users.push(user);
+}
+
+function getUser(email = null, id = null) {
+    if(!email && !id) return null;
+    if (id) return users.find(user => user.id === id);
+    return users.find(user => user.email === email);
+}
+
+function updateUser(id, user){
+    const index = users.findIndex(user => user.id === id);
+    user.id = id;
+    users[index] = user;
+}
+
+function setSocketID(id, socketID){
+    const user = getUser("", id);
+    user.socketID = socketID;
+    updateUser(id, socketID);
+}
+
+module.exports = { createUser, saveUser, getUser, updateUser, setSocketID };
